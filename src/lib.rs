bec417f0700f9ee0690b058e0432b808048ddd22
Marco Rotili <marco.livio.rotili@gmail.com>
2021-05-20 19:31:14 +0200

    add doc example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # Nakama rust client guide
+//! ## Setup
+//! ## Authenticate
+//! 1. Build an instance of the client
+//! ```
+//! # use nakama_rs::client::{DefaultClient, Client};
+//! # use nakama_rs::http_adapter::RestHttpAdapter;
+//! # use futures::executor::block_on;
+//! # use std::collections::HashMap;
+//! let adapter = RestHttpAdapter::new("http://127.0.0.1", 7350);
+//! let client = DefaultClient::new(adapter);
+//! ```
+//! 2. Authenticate a user
+//! ```
+//! # use nakama_rs::client::{DefaultClient, Client};
+//! # use nakama_rs::http_adapter::RestHttpAdapter;
+//! # use futures::executor::block_on;
+//! # use std::collections::HashMap;
+//! # let adapter = RestHttpAdapter::new("http://127.0.0.1", 7350);
+//! # let client = DefaultClient::new(adapter);
+//! block_on(async {
+//!     client.authenticate_device("testdeviceid", None, true, HashMap::new()).await;
+//! })
+//! ```
+//! ## Sessions
+//! ## Send requests
+//! ## Socket messages
+//! ## Handle events
+//! ## Logs and errors
+//! ## Full example
 mod api_gen;
 
 pub mod config;
